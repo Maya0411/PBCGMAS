817cd92d970e31772b2f89a38abb2043691aeb65
Sumayyah <86755241+Maya0411@users.noreply.github.com>
2022-03-29 10:38:57 +0800

    Add files via upload

---
+++ number.php
@@ -0,0 +1,29 @@
+<?php 
+
+$num1 = 12;
+$num2 = 23;
+$num3 = 7.2;
+
+echo "<h2>Assalammualaikum</h2>";
+echo "<p>Selamat Sejahtera</p>";
+
+echo "<h2>2SVMKPD2018</h2>";
+echo "<p>Pengujian Atur cara</p>";
+
+echo "Hasil Operasi $num1 + $num2 = ";
+echo $num1+$num2;
+echo "<br>";
+
+echo "Hasil Operasi $num1 - $num2 = ";
+echo $num1-$num2;
+echo "<br>";
+
+echo "Hasil Operasi $num1 * $num2 = ";
+echo $num1*$num2;
+echo "<br>";
+
+echo "Hasil Operasi $num1 / $num2 = ";
+echo $num1/$num2;
+echo "<br>"
+
+ ?>
